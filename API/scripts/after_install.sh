64010e79939ebd0948520aaa2f39790fe997e076
Martin Morente Vargas <martin.dev45@gmail.com>
2026-05-05 17:53:13 +0200

    Update
    Deploy finished

---
+++ API/scripts/after_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# TODO desencrypt the production env
+
+APP_KEY=$(aws ssm get-parameter \
+  --name "/backend/app-key" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text \
+  --region us-east-1)
+
+cd ~/home/ubuntu/InfernumAPI
+
+docker compose up -d --build
+
+sleep 5
+
+docker compose exec -T Laravel composer install --no-dev --optimize-autoloader
+
+docker compose exec -T Laravel php artisan env:desencrypt --env=production --key=${APP_KEY}
+
+docker compose exec -T Laravel npm install
+
+docker compose exec -T Laravel php artisan optimize:clear && docker compose exec -T Laravel php artisan optimize
+
+docker compose exec -T Laravel php artisan migrate
+
+docker compose exec -T Laravel php artisan serve --host 0.0.0.0
