6b4f51263e63e821b234a96962e2c659ecec3542
Martin Morente Vargas <martin.dev45@gmail.com>
2026-05-12 09:29:25 +0200

    Update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,20 +8,22 @@ APP_KEY=$(aws ssm get-parameter \
   --output text \
   --region us-east-1)
 
-cd /var/www/html/public/Infernum-API/API/setup
+cd /var/www/html/public/Infernum-API/API
 
-docker build -f Dockerfile.base -t base_image .
+docker build -f setup/Dockerfile.base -t base_image .
 
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f setup/docker-compose.prod.yml up -d --build
 
-docker compose exec -T Laravel composer install --no-dev --optimize-autoloader
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel composer install --no-dev --optimize-autoloader
 
-docker compose exec -T Laravel php artisan env:descrypt --env=production --key=${APP_KEY}
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel php artisan env:decrypt --env=production --key="${APP_KEY}"
 
-docker compose exec -T Laravel npm install
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel npm install
 
-docker compose exec -T Laravel php artisan optimize:clear && docker compose exec -T Laravel php artisan optimize
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel php artisan optimize:clear
 
-docker compose exec -T Laravel php artisan migrate
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel php artisan optimize
 
-docker compose exec -T Laravel sudo service apache2 reload
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel php artisan migrate
+
+docker compose -f setup/docker-compose.prod.yml exec -T Laravel service apache2 reload
