13be42459ec5a5224c4d0a1ef0d98773b0577782
Martin Morente Vargas <martin.dev45@gmail.com>
2026-05-12 09:59:39 +0200

    Update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -24,6 +24,9 @@ docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public/
 docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public/Infernum-API app \
   php artisan env:decrypt --env=production --key="${APP_KEY}"
 
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/app app \
+  cp .env.production .env
+
 docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public/Infernum-API app \
   npm install
 
