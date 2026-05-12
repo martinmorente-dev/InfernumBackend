c83436b055feeb8fbb844b335098c71ca9fa6108
Martin Morente Vargas <martin.dev45@gmail.com>
2026-05-12 11:57:02 +0200

    Update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter --name "/infernum/MYSQL_ROOT_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
-MYSQL_USER=$(aws ssm get-parameter --name "/infernum/MYSQL_USER" --with-decryption --query "Parameter.Value" --output text)
-MYSQL_PASSWORD=$(aws ssm get-parameter --name "/infernum/MYSQL_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
-MYSQL_DATABASE=$(aws ssm get-parameter --name "/infernum/MYSQL_DATABASE" --with-decryption --query "Parameter.Value" --output text)
+MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter --name "/infernum/MYSQL_ROOT_PASSWORD" --with-decryption --query "Parameter.Value" --output text --region us-east-1)
+MYSQL_USER=$(aws ssm get-parameter --name "/infernum/MYSQL_USER" --with-decryption --query "Parameter.Value" --output text --region us-east-1)
+MYSQL_PASSWORD=$(aws ssm get-parameter --name "/infernum/MYSQL_PASSWORD" --with-decryption --query "Parameter.Value" --output text --region us-east-1)
+MYSQL_DATABASE=$(aws ssm get-parameter --name "/infernum/MYSQL_DATABASE" --with-decryption --query "Parameter.Value" --output text --region us-east-1)
 
 rm -fr /var/www/html/public/Infernum-API/*
 
