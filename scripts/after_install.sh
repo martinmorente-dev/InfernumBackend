40340837593ac096441ae7ae2cb7d8d13e096905
Martin Morente Vargas <martin.dev45@gmail.com>
2026-05-12 09:47:39 +0200

    Update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -18,16 +18,23 @@ docker build -f setup/Dockerfile.base -t base_image .
 
 docker compose -f setup/docker-compose.prod.yml up -d --build
 
-docker compose -f setup/docker-compose.prod.yml exec -T app composer install --no-dev --optimize-autoloader
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public app \
+  composer install --no-dev --optimize-autoloader
 
-docker compose -f setup/docker-compose.prod.yml exec -T app php artisan env:decrypt --env=production --key="${APP_KEY}"
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public app \
+  php artisan env:decrypt --env=production --key="${APP_KEY}"
 
-docker compose -f setup/docker-compose.prod.yml exec -T app npm install
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public app \
+  npm install
 
-docker compose -f setup/docker-compose.prod.yml exec -T app php artisan optimize:clear
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public app \
+  php artisan optimize:clear
 
-docker compose -f setup/docker-compose.prod.yml exec -T app php artisan optimize
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public app \
+  php artisan optimize
 
-docker compose -f setup/docker-compose.prod.yml exec -T app php artisan migrate
+docker compose -f setup/docker-compose.prod.yml exec -T -w /var/www/html/public app \
+  php artisan migrate --force
 
-docker compose -f setup/docker-compose.prod.yml exec -T app service apache2 reload
+docker compose -f setup/docker-compose.prod.yml exec -T app \
+  service apache2 reload
