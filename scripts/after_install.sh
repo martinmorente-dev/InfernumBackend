970a73b15a06a3388100f598f16ad207900dc9a7
Martin Morente Vargas <martin.dev45@gmail.com>
2026-06-07 22:35:10 +0200

    Update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -74,9 +74,6 @@ docker compose -f setup/docker-compose.prod.yml exec -T -u root -w /var/www/html
 docker compose -f setup/docker-compose.prod.yml exec -T -u www-data -w /var/www/html/public/Infernum-API app \
   php artisan config:clear
 
-docker compose -f setup/docker-compose.prod.yml exec -T -u www-data -w /var/www/html/public/Infernum-API app \
-  php artisan livewire:publish --assets --ansi
-
 # Habilitar Docker al inicio y recargar Apache
 sudo systemctl enable docker
 
